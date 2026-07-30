9decab67d5429ba76f76176baa7fd13b00c81d45
zhesphere <lyzcrease@outlook.com>
2026-07-30 23:58:39 +0800

    更新公告和一些动态

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,10 +5,10 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	content: "欢迎四方来客，八方来财！",
 
 	// 是否允许用户关闭公告
-	closable: true,
+	closable: false,
 
 	link: {
 		// 启用链接
